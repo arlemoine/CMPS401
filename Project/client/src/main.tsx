ede8ad02ffbd8a499eab5ab330c920b51dd20a37
adamrodi <adam.rodi@selu.edu>
2025-10-09 16:32:40 -0500

    add frontend WS connection

--- Project/client/src/main.tsx
+++ Project/client/src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
-import App from './App.tsx';
+import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme="dark">
-      <Notifications />
-      <App />
+        <Notifications />
+        <App />
     </MantineProvider>
   </React.StrictMode>
 );
